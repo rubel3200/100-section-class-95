eca7f2e657031f76996e4fe53158e1f6a5f3b379
rubel3200 <rubelahmed3200@gmail.com>
2017-11-12 21:22:11 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".slider-area").owlCarousel({
+            items:1,
+            loop:true,
+            nav:false,
+            dots:true,
+            autoplay:false
+            
+        });
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
